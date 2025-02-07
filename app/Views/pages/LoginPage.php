e4a1c3ac6577d3037bed856e4dec3d3c5f37763c
Rifqin-11 <rifqinaufal9009@gmail.com>
2025-02-07 16:00:34 +0700

    change login page

--- app/Views/pages/LoginPage.php
+++ app/Views/pages/LoginPage.php
@@ -3,30 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
-    <script src="https://unpkg.com/lucide@latest"></script>
-    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
-    <style type="text/tailwindcss">
-      @theme {
-        --color-primary: #084E8F;
-        --color-secondary: #f9f9f9;
-        --color-button: #2563eb;
-        --color-text-100: #7E7E7E;
-        --color-text-200: #414141;
-        --color-text-600: #364153;
-      }
-    </style>
+    <title>Desnet | Login</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="flex items-center justify-center h-screen bg-gradient-to-r from-gray-200 to-blue-200">
-    <div class="bg-white rounded-3xl shadow-lg relative overflow-hidden w-[768px] max-w-full min-h-[480px]">
-        <div class="absolute w-full h-full opacity-100 z-10 flex flex-col justify-center items-center px-10 text-center sign-in">
-            <h1 class="text-2xl font-semibold">Sign In</h1>
-            <span class="text-sm">use your email for Login</span>
-            <input type="email" placeholder="Email" class="mt-10 w-full bg-gray-200 my-2 p-2 rounded-lg border border-gray-300 outline-none">
-            <input type="password" placeholder="Password" class="w-full bg-gray-200 my-2 p-2 rounded-lg border border-gray-300 outline-none">
-            <a href="#" class="text-xs text-gray-600 mt-2 hover:underline">Forget Password?</a>
-            <button class="bg-purple-700 text-white px-6 py-2 rounded-lg mt-4 hover:bg-purple-800 transition">Sign In</button>
+<body class="bg-gray-100 flex flex-col min-h-screen">
+    <nav class="bg-white p-7">
+        <a href="<?= base_url('/'); ?>" class="container flex justify-between items-center">
+            <img src="<?= base_url('desnetLogo.png') ?>" alt="desnet logo" class="w-24">
+        </a>
+    </nav>
+
+    <div class="flex flex-grow justify-center items-center">
+        <div class="bg-white flex flex-col justify-center items-center w-full max-w-lg px-8 py-20 rounded-xl shadow-lg">
+            <h2 class="text-black text-3xl font-bold text-center">Welcome Back!</h2>
+            <p class="text-gray-400 text-sm text-center pb-6">Please enter your details</p>
+
+            <form action="/auth" method="post" class="w-full pt-8">
+                <input type="text" name="username" class="bg-white border text-black text-sm rounded-lg w-full p-2.5 mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Username" required>
+
+                <input type="password" name="password" class="bg-white border text-black text-sm rounded-lg w-full p-2.5 mb-6 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Password" required>
+
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center">Login</button>
+            </form>
         </div>
     </div>
+
+    <footer class="bg-white text-gray-400 font-normal text-center py-3 shadow-md">
+        <p>© 2025 PT Des Teknologi Informasi All Rights Reserved</p>
+    </footer>
 </body>
 </html>
