de8127375763b07cb89a09a3e188c540c7e03d56
Rifqi Naufal <92136877+Rifqin-11@users.noreply.github.com>
2025-03-11 09:05:36 +0700

    feat(UI): add bg toggle to change password

--- app/Views/pages/EmployeeSettings.php
+++ app/Views/pages/EmployeeSettings.php
@@ -276,10 +276,10 @@
                       <div class="col-span-2">
                         <label for="new_password" class="block mb-2 text-sm font-medium text-gray-900">New Password</label>
                         <div class="flex items-center mb-2">
-                            <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 focus:ring-2">
+                            <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-blue-300 border-gray-300 rounded-sm focus:ring-blue-500 focus:ring-2">
                             <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900">Change Password?</label>
                         </div>
-                        <input type="password" name="new_password" id="new_password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="Enter new password" required>
+                        <input type="password" name="new_password" id="new_password" class="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="Enter new password" required>
                       </div>
                     </div>
 
@@ -328,6 +328,7 @@
           
           checkbox.addEventListener('change', function() {
             newPasswordInput.disabled = !this.checked;
+            newPasswordInput.classList.toggle('bg-gray-200');
           });
         });
       // Close Modal
